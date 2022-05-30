a993df124f7389b46181378daf77cd9dcdab5f8e
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-30 15:29:05 +0900

    カウントを管理するクラスファイルの生成

---
+++ PracticePassToValueByCallback/CountManager.swift
@@ -0,0 +1,16 @@
+//
+//  CountManager.swift
+//  PracticePassToValueByCallback
+//
+//  Created by Johnny Toda on 2022/05/30.
+//
+
+import Foundation
+
+class CountManager {
+    private(set) var count = 0
+
+    func increment() {
+        count += 1
+    }
+}
