ba463a22985715dc29e85dfa12ffda21ba93f123
0102johnny <rt2824.recruit.1022@gmail.com>
2022-05-31 15:38:19 +0900

    【Next】CountPlusButtonをタップするとcountが+1され、CountLabelに現在のCountの値が反映される処理の実装

--- PracticePassToValueByCallback/NextViewController.swift
+++ PracticePassToValueByCallback/NextViewController.swift
@@ -8,22 +8,17 @@
 import UIKit
 
 class NextViewController: UIViewController {
-
+    private let countManager = CountManager()
+    @IBOutlet weak var countLabel: UILabel!
+    @IBAction func didTapButton(_ sender: Any) {
+        countManager.increment()
+        countLabel.text = countManager.count.description
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
